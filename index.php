6ef3a6cbde50423bd823ffe99685fef43270bf53
TheOnlySmartBoy <o2jose43@gmail.com>
2020-01-31 13:47:56 +0300

    Updated files to php

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
           <div class="col-md-6">
             <ul class="block-45-list">
               <li><a href="#">Home</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Ministries</a></li>
+              <li><a href="about.php">About Us</a></li>
+              <li><a href="ministry.php">Ministries</a></li>
             </ul>
           </div>
           <div class="col-md-6 text-md-right">
@@ -48,7 +48,8 @@
      
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-          <a class="navbar-brand absolute" href="index.php"><span class="fa fa-heart text-primary"></span>  </a>
+            
+            <a class="navbar-brand absolute" href="index.php"><img src="images/logo.gif" alt="" style="width: 100px; height: 100px;"/> <span class="text-primary">Bethlehem Sda</span>  </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -56,38 +57,38 @@
           <div class="collapse navbar-collapse navbar-light" id="navbarsExample05">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="ministry.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ministries</a>
+                <a class="nav-link dropdown-toggle" href="ministry.php" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ministries</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown04">
-                  <a class="dropdown-item" href="ministry.html">Outreach Ministry</a>
-                  <a class="dropdown-item" href="ministry.html">Men's Ministry</a>
-                  <a class="dropdown-item" href="ministry.html">Women's Ministry</a>
-                  <a class="dropdown-item" href="ministry.html">Children's Ministry</a>
-                  <a class="dropdown-item" href="ministry.html">Youth Ministry</a>
+                  <a class="dropdown-item" href="ministry.php">Outreach Ministry</a>
+                  <a class="dropdown-item" href="ministry.php">Men's Ministry</a>
+                  <a class="dropdown-item" href="ministry.php">Women's Ministry</a>
+                  <a class="dropdown-item" href="ministry.php">Children's Ministry</a>
+                  <a class="dropdown-item" href="ministry.php">Youth Ministry</a>
                 </div>
 
               </li>
 
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="sermon.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sermons</a>
+                <a class="nav-link dropdown-toggle" href="sermon.php" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sermons</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown05">
-                  <a class="dropdown-item" href="sermon.html">Daily Sermon</a>
-                  <a class="dropdown-item" href="sermon.html">Music</a>
-                  <a class="dropdown-item" href="sermon.html">Audio</a>
-                  <a class="dropdown-item" href="sermon.html">Video</a>
+                  <a class="dropdown-item" href="sermon.php">Daily Sermon</a>
+                  <a class="dropdown-item" href="sermon.php">Music</a>
+                  <a class="dropdown-item" href="sermon.php">Audio</a>
+                  <a class="dropdown-item" href="sermon.php">Video</a>
                 </div>
 
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="events.html">Events</a>
+                <a class="nav-link" href="events.php">Events</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="about.html">About</a>
+                <a class="nav-link" href="about.php">About</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact</a>
+                <a class="nav-link" href="contact.php">Contact</a>
               </li>
             </ul>
             
